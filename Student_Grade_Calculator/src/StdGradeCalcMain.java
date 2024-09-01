4b44113d4d770795d4c4a113eaa9d9313d906233
YogeshBhandari369 <YogeshBhandari369l@gmail.com>
2024-09-01 17:33:35 +0530

    added avg and grade caculator

--- Student_Grade_Calculator/src/StdGradeCalcMain.java
+++ Student_Grade_Calculator/src/StdGradeCalcMain.java
@@ -5,24 +5,46 @@ public class StdGradeCalcMain {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		
-		//Enter total number of subjects to calculate their marks
+		//Enter total number of subjects
 		System.out.println("Enter total number of subjects:");
 		int noOfSub=sc.nextInt();
 		int totalMarks=0;
 		
+		//Calculate sum of all subjects
 		for(int i=0;i<noOfSub;i++) {
-			System.out.println("Enter marks for subject Number "+(i+1)+" (out of 100) :");
+			System.out.println("Enter marks for subject "+(i+1)+" (out of 100) :");
 			int marks=sc.nextInt();
 			//Input Validation
 			while(marks<0||marks>100) {
 				System.out.println("Invalid marks please enter between 0 and 100");
-				System.out.println("Enter marks for subject Number "+(i+1)+" (out of 100) :");
+				System.out.println("Enter marks for subject "+(i+1)+" (out of 100) :");
 				marks=sc.nextInt();
 			}
 			totalMarks+=marks;
 		}
 		System.out.println("Student Results:");
 		System.out.println("Total marks obtained in all subjects: "+totalMarks);
+		int averagePercentage=totalMarks/noOfSub;
+		System.out.println("Average Percentage: "+averagePercentage);
+		if(averagePercentage>=90) {
+			System.out.println("Your grade: A+");
+		}else if(averagePercentage>=80) {
+			System.out.println("Your grade: A");
+		}else if(averagePercentage>=70) {
+			System.out.println("Your grade: B+");
+		}else if(averagePercentage>=60) {
+			System.out.println("Your grade: B");
+		}else if(averagePercentage>=50) {
+			System.out.println("Your grade: C+");
+		}else if(averagePercentage>=40) {
+			System.out.println("Your grade: C");
+		}else if(averagePercentage>=30) {
+			System.out.println("Your grade: D+");
+		}else if(averagePercentage>=20){
+			System.out.println("Your grade: D");
+		}else {
+			System.out.println("Your grade: E");
+		}
 		sc.close();
 	}
 }
